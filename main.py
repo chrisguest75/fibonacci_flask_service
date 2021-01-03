c28528ebc0a116404b60e2bd2e448830fa07ca6a
Chris Guest <chris.guest@condenastint.com>
2021-01-03 16:16:38 +0000

    Add metrics endpoint

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from flask import Flask, render_template
 import connexion
+from prometheus_flask_exporter import ConnexionPrometheusMetrics
+
 
 # Create the application instance
-app = connexion.App(__name__, specification_dir='./')
+app = connexion.FlaskApp(__name__, specification_dir='./')
+metrics = ConnexionPrometheusMetrics(app)
 
 # Read the swagger.yml file to configure the endpoints
 app.add_api('swagger.yaml')
+
 
 @app.route("/")
 def home():
     return render_template("fibonacci.html")
 
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
